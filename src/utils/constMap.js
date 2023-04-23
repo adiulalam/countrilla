9bc7e4bf5cac848ae6a7e6809e1b99809d142097
Adiul Alam Adil <adiulalam@gmail.com>
2023-04-23 22:06:59 +0100

    region

---
+++ src/utils/constMap.js
@@ -0,0 +1,8 @@
+export const region = [
+	{ name: "Europe", value: "europe" },
+	{ name: "Americas", value: "americas" },
+	{ name: "Africa", value: "africa" },
+	{ name: "Oceania", value: "oceania" },
+	{ name: "Asia", value: "asia" },
+	{ name: "Antarctic", value: "antarctic" },
+];
