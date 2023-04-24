8a8b6dd296bd04b4150939c1885ec3180a988f44
Adiul Alam Adil <adiulalam@gmail.com>
2023-04-24 23:58:02 +0100

    fix: country area

--- src/pages/country/[country].jsx
+++ src/pages/country/[country].jsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 const Country = () => {
 	const router = useRouter();
 	const { country } = router.query;
-	console.log("🚀 ~ file: index.js:6 ~ Post ~ router.query:", router.query);
 
 	return <p>Post: {country}</p>;
 };
